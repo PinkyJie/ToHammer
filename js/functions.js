e941fbb1042902105480b6d4737152187224a2bd
wenbo.jie <wenbo.jie@samsung.com>
2013-05-20 11:17:05 +0800

    add js for music

--- js/functions.js
+++ js/functions.js
@@ -21,16 +21,16 @@ $(function() {
     $("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
     $("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
     setInterval(function() {
-        garden.render()
+        garden.render();
     },
-    Garden.options.growSpeed)
+    Garden.options.growSpeed);
 });
 
 function getHeartPoint(c) {
     var b = c / Math.PI;
     var a = 19.5 * (16 * Math.pow(Math.sin(b), 3));
     var d = -20 * (13 * Math.cos(b) - 5 * Math.cos(2 * b) - 2 * Math.cos(3 * b) - Math.cos(4 * b));
-    return new Array(offsetX + a, offsetY + d)
+    return new Array(offsetX + a, offsetY + d);
 }
 function startHeartAnimation() {
     var c = 100;
@@ -44,7 +44,7 @@ function startHeartAnimation() {
             var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
             if (j < Garden.options.bloomRadius.max * 1.3) {
                 e = false;
-                break
+                break;
             }
         }
         if (e) {
@@ -53,7 +53,7 @@ function startHeartAnimation() {
         }
         if (d >= 30) {
             clearInterval(a);
-            showMessages()
+            showMessages();
         } else {
             d += 0.2
         }
@@ -70,26 +70,26 @@ function startHeartAnimation() {
             var e = setInterval(function() {
                 var f = c.substr(b, 1);
                 if (f == "<") {
-                    b = c.indexOf(">", b) + 1
+                    b = c.indexOf(">", b) + 1;
                 } else {
-                    b++
+                    b++;
                 }
                 d.html(c.substring(0, b) + (b & 1 ? "_": ""));
                 if (b >= c.length) {
-                    clearInterval(e)
+                    clearInterval(e);
                 }
             },
             200)
         });
-        return this
+        return this;
     }
 })(jQuery);
 function showMessages() {
-    showLoveU()
+    showLoveU();
 }
 function adjustCodePosition() {
-    $("#code").css("margin-top", ($("#garden").height() - $("#code").height()) / 2)
+    $("#code").css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
 }
 function showLoveU() {
-    $("#loveu").fadeIn(3000)
+    $("#loveu").fadeIn(3000);
 };
